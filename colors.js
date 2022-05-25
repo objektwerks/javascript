d6327af88ec0c2324e3b2d024ad8bc8fee961103
objektwerks <objektwerks@runbox.com>
2022-05-25 13:00:16 -0400

    refactored colors.js

--- colors.js
+++ colors.js
@@ -10,20 +10,16 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var image = context.createImageData(imageWidth, imageHeight);
 
-drawColors(context, image);
-
-function drawColors(context, image) {
-  for (let i = 0; i < image.data.length; i += 4) {
-    var x = image.data.getX();
-    var y = image.data.getY();
-    if (x < imageWidth / 2) {
-      image.data[i + red] = 255;
-    } 
-    if (y > imageHeight / 2) {
-      image.data[i + blue] = 255;
-    } else {
-      image.data[i + green] = 255;
-    }
+for (let i = 0; i < image.data.length; i += 4) {
+  var x = image.data.getX();
+  var y = image.data.getY();
+  if (x < imageWidth / 2) {
+    image.data[i + red] = 255;
+  } 
+  if (y > imageHeight / 2) {
+    image.data[i + blue] = 255;
+  } else {
+    image.data[i + green] = 255;
   }
-  context.putImageData(image, 25, 25);
 }
+context.putImageData(image, 25, 25);
