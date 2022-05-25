2c051ed5a7851c0363acbbf0d66e4f83493ccf7a
objektwerks <objektwerks@runbox.com>
2022-05-25 14:12:26 -0400

    refactored colors.js

--- colors.js
+++ colors.js
@@ -13,17 +13,20 @@ var image = context.createImageData(imageWidth, imageHeight);
 console.assert(imageWidth === image.width);
 console.assert(imageHeight === image.height);
 
+const halfImageWidth = imageWidth / 2;
+const halfImageHeight = imageHeight / 2;
+
 for (var i = 0; i < image.data.length; i += 4) {
   var x = (i / 4) % image.width;
   var y = Math.floor((i / 4) / image.width);
 
   image.data[i + alpha] = 255;
 
-  if (x < imageWidth / 2) {
+  if (x < halfImageWidth) {
     image.data[i + red] = 255;
   }
 
-  if (y > imageHeight / 2) {
+  if (y > halfImageHeight) {
     image.data[i + blue] = 255;
   } else {
     image.data[i + green] = 255;
