99006975e2168fbb391976bffffa959c9a614edf
objektwerks <objektwerks@runbox.com>
2022-05-25 13:54:34 -0400

    added image width and height assert

--- colors.js
+++ colors.js
@@ -10,18 +10,18 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var image = context.createImageData(imageWidth, imageHeight);
 
-console.log("image width: " + image.width + " height: " + image.height);
-console.log("image data length: " + image.data.length);
+console.assert(imageWidth === image.width);
+console.assert(imageHeight === image.height);
 
 for (var i = 0; i < image.data.length; i += 4) {
   var x = (i / 4) % image.width;
   var y = Math.floor((i / 4) / image.width);
   image.data[i + alpha] = 255;
 
-  if (x < image.width / 2) {
+  if (x < imageWidth / 2) {
     image.data[i + red] = 255;
   } 
-  if (y > image.height / 2) {
+  if (y > imageHeight / 2) {
     image.data[i + blue] = 255;
   } else {
     image.data[i + green] = 255;
