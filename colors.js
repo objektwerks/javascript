7d51500ba7ebccb04f9c79d1e4047260a28b74c0
objektwerks <objektwerks@runbox.com>
2022-05-25 12:50:43 -0400

    refactored image width and height

--- colors.js
+++ colors.js
@@ -1,12 +1,14 @@
-
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var image = context.createImageData(200, 200);
+const imageWidth = 200;
+const imageHeight = 200;
 
 const red = 0;
 const green = 1;
 const blue = 2;
 const alpha = 3;
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var image = context.createImageData(imageWidth, imageHeight);
 
 drawColors(context, image);
 
@@ -14,10 +16,10 @@ function drawColors(context, image) {
   for (let i = 0; i < image.data.length; i += 4) {
     var x = image.data.getX();
     var y = image.data.getY();
-    if (x < image.getWidth() / 2){
+    if (x < imageWidth / 2){
       image.data[i + red] = 255;
     } 
-    if (y > image.getHeight() / 2){
+    if (y > imageHeight / 2){
       image.data[i + blue] = 255;
     } else {
       image.data[i + green] = 255;
